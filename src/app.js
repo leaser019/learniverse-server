5b7758488c87f8f839c4ee420e73ef1ed490e1f8
leaser019 <itcsiu21136@student.hcmiu.edu.vn>
2025-05-25 17:48:06 +0700

    fix: enhance CORS middleware configuration and add preflight support

--- src/app.js
+++ src/app.js
@@ -12,14 +12,27 @@ const app = express()
 
 // Middleware
 app.use(helmet())
-app.use(cors())
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://learniverse-client.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
+    credentials: true
+  })
+)
 app.use(morgan('dev'))
 app.use(compression())
 app.use(requestLogger)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-api-key')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.sendStatus(204)
+})
 
 // Database
 require('./dbs/init.mongodb')
@@ -27,6 +40,7 @@ checkOverload()
 
 // Routes
 app.use('/', require('./router'))
+
 
 // Error handling middleware
 app.use((req, res, next) => {
