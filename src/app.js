77eb1512bb73c987626bb509f8684472ed174837
leaser019 <itcsiu21136@student.hcmiu.edu.vn>
2025-05-25 16:26:44 +0700

    fix: update CORS middleware to explicitly define methods and allowed headers

--- src/app.js
+++ src/app.js
@@ -20,6 +20,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(
   cors({
     origin: ['http://localhost:3000', 'https://learniverse-client.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   })
 )
