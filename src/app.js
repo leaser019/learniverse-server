0111a3b1730140d930a212d73446935b1eae5089
leaser019 <itcsiu21136@student.hcmiu.edu.vn>
2025-07-10 21:00:34 +0700

    fix: update CORS middleware to use static origin configuration

--- src/app.js
+++ src/app.js
@@ -1,27 +1,44 @@
 'use strict'
 const express = require('express')
 const morgan = require('morgan')
-const helmet = require('helmet')
 const compression = require('compression')
 const { requestLogger } = require('./configs/config.logger')
 const { checkOverload } = require('./helpers/check.connection')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
+const allowedOrigins = [
+  'https://learniverse-client.vercel.app',
+  'http://localhost:3000',
+  'https://waf-fguard.vercel.app'
+]
 
 // Middleware
-// app.use(helmet({}))
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  })
+)
 app.use(morgan('dev'))
 app.use(compression())
 app.use(requestLogger)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
 // Database
 require('./dbs/init.mongodb')
 checkOverload()
 
 // Routes
 app.use('/', require('./router'))
+
 
 // Error handling middleware
 app.use((req, res, next) => {
